cc96cf3521b0602fb6bd587a3ff77e42dc1a24ca
root <root@DESKTOP-GMQO0O9.localdomain>
2020-04-25 22:26:11 -0400

    4/26 goals

--- README.md
+++ README.md
@@ -6,14 +6,19 @@
 
 # V2: Add the backend -> Calendar #
 Probably gonna look into some kind of sql (postgres?) or mongodb, add a calendar to track multiple sessions/days  
-- [ ] implement cookies through react, forget about back end for now. Might not even need it for the version I'm currently thinking of  
-which could work like how the animal crossing thing did
-- [ ] track the sessions over dates
-- [ ] make table items editable and deletable
+- [x] implement storage in localstorage
+- [ ] refresh every single day? Maybe I do have to connect it to a backend...
+- [x] make table items editable and deletable
 - [ ] use google calendar api?
 
 # Current Goals: #
-- [ ] make site work with cookies
+4/25
+- [x] make site work with local storage -> removing the backend, not really necessary in this case
+4/26
+- [ ] Fix spacing at the top
+- [ ] Add a time section tracker
+- [ ] Make popup for editing the base times
+Future
 - [ ] add sound
 
 # Extra #
--- server/routes.js
+++ server/routes.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser')
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 const { Pool, Client } = require('pg')
-const connectionString = 'postgresql://tester2:testing@localhost:5432/pomodoro'
+const connectionString = 'postgresql://testuser:test@localhost:5432/pomodoro'
 const pool = new Pool({
   connectionString: connectionString,
 })
@@ -15,11 +15,14 @@ app.use(cors());
 app.use(bodyParser.json())
 
 const createTasks = (req, res) => {
+    console.log("hello");
     const command = 'INSERT INTO task(task_name, count, breaks) VALUES(\'' + req.body.task_name + "\'" + ', 0, 0)'
     pool.query(command, (error, results) => {
         if (error) {
+          console.log(error);
           throw error
         }
+        console.log(results)
         res.status(200).json(results.rows)
       })
 }
@@ -86,6 +89,6 @@ app.route('/api/tasks/:id').put(updateTask).delete(deleteTask)
 //     res.status(200).json(1)
 // })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => `Server running on port ${PORT}`);
